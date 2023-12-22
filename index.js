17e6c65ca63e6e5ec4c4da78d687a90184b83b78
muhammad-shahin <muhammadshahin002@gmail.com>
2023-12-22 06:54:02 +0600

    new database credentials added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Assignment Data Will Add Soon');
 });
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ko1sj04.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clucrnq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
